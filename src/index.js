2b76af869250d3f5852d766aa3c2117c0541f9ba
Niko Yea <nikoyea14@gmail.com>
2024-03-12 01:30:40 +0100

    page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import './styles/main.scss'
 import './assets/_sprite.svg'
+
+import './js/carousels'
---
+++ src/js/carousels.js
@@ -0,0 +1,21 @@
+import $ from 'jquery'
+import slick from './slick.min'
+
+$(document).ready(function(){
+  $('.services').slick({
+    slidesToShow: 3 ,
+    slidesToScroll: 1,
+    arrows: false,
+    centerMode: true,
+    centerPadding: 0,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          centerPadding: '50px',
+        }
+      },
+    ]
+  })
+})
