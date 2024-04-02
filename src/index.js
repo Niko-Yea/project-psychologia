c0933d205b5955b70c4fbe9b589e43bbbc73ec58
Niko Yea <nikoyea14@gmail.com>
2024-04-02 23:13:36 +0200

    fix review notes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ import './styles/main.scss'
 import './assets/_sprite.svg'
 
 import './js/carousels'
+
+const scrollToTopImg = document.querySelector('.scroll-to-top')
+scrollToTopImg.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+})
